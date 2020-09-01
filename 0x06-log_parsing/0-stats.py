9ab8922c00e189efe4f672252a70d4cb558aa5ae
edin93 <il_matador@hotmail.fr>
2020-09-01 00:45:22 +0000

    log parsing updated.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,9 +13,16 @@ try:
     for i, line in enumerate(sys.stdin, 1):
         infos = line.split(' ')
         if len(infos) > 2:
-            file_size += int(infos[-1])
-            if infos[-2] in status_codes:
-                status_codes[infos[-2]] += 1
+            try:
+                file_size += int(infos[-1])
+            except Exception:
+                raise Exception
+            try:
+                if (
+                        infos[-2] in status_codes and
+                        is_instance(int(infos[-2], int))
+                ):
+                    status_codes[infos[-2]] += 1
 
         if i % 10 == 0:
             print('File size: {}'.format(file_size))
