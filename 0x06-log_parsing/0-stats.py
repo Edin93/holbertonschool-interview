8a1561813bf90d3bc9cb3759410459eb3dae1f4e
edin93 <il_matador@hotmail.fr>
2020-09-01 00:01:14 +0000

    update log parsing.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,9 +7,12 @@ status_codes = {
     '200': 0, '301': 0, '400': 0, '401': 0,
     '403': 0, '404': 0, '405': 0, '500': 0
 }
+
 file_size = 0
+i = 1
+
 try:
-    for i, line in enumerate(sys.stdin, 1):
+    for line in sys.stdin:
         split_line = line.split()
         if len(split_line) > 2:
             infos = [int(x) for x in split_line[-2:]]
@@ -22,6 +25,7 @@ try:
             for k in sorted(status_codes):
                 if status_codes[k] != 0:
                     print('{}: {}'.format(k, status_codes[k]))
+        i += 1
 except Exception:
     pass
 finally:
