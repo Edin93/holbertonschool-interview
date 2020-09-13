6ea264bf3143d4dc17424411224a4594e45a1982
edin93 <il_matador@hotmail.fr>
2020-09-13 21:06:58 +0000

    duplicate user input.;

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -7,23 +7,22 @@
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long x, y;
-	unsigned long div = 1;
+	unsigned long x, y, num = n, div = 1;
 
-	if (n < 10)
+	if (num < 10)
 		return (1);
 
-	while (n / (div * 10) >= 1)
+	while (num / (div * 10) >= 1)
 		div *= 10;
 
 	while (div > 1)
 	{
-		x = n / div;
-		y = n % 10;
+		x = num / div;
+		y = num % 10;
 		if (x != y)
 			return (0);
-		n = n - (x * div);
-		n = n / 10;
+		num = num - (x * div);
+		num = num / 10;
 		div = div / 100;
 	}
 	return (1);
