4e6a15fb8440fb2e9a4fda10729710c077d95311
edin93 <il_matador@hotmail.fr>
2020-09-15 09:59:48 +0000

    updat palindrom integer algo

--- 0x08-palindrome_integer/algo1.c
+++ 0x08-palindrome_integer/algo1.c
@@ -7,17 +7,22 @@
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long div = 1;
+	unsigned long x, y, num = n, div = 1;
 
-	while (n / (div * 10) >= 1)
+	if (num < 10)
+		return (1);
+
+	while (num / (div * 10) >= 1)
 		div *= 10;
 
 	while (div > 1)
 	{
-		if (n / div != n % 10)
+		x = num / div;
+		y = num % 10;
+		if (x != y)
 			return (0);
-		n = n - ((n / div) * div);
-		n = n / 10;
+		num = num - (x * div);
+		num = num / 10;
 		div = div / 100;
 	}
 	return (1);
