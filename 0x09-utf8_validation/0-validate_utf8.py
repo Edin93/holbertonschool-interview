0fb4920ba4055dc451da457f64040d085e173ae4
edin93 <il_matador@hotmail.fr>
2020-09-21 00:42:53 +0000

    update utf-8 validation algo.

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,15 +8,14 @@ def validUTF8(data):
     """
     Check if given data is a valid UTF-8 encoding.
     """
-    r1 = range(0, 128)
-    r2 = range(49280, 57280)
-    r3 = range(14712960, 15712192)
-    r4 = range(4034953344, 4156538816)
-    i = 0
 
-    while (i < len(data)):
-        n = data[i]
-        if n > 127:
-            return False
-        i += 1
+    for n in data:
+        if n in range(0, 128):
+            pass
+        else:
+            b = '{:b}'.format(n)
+            b = b[::-1]
+            b = (b[:8])[::-1]
+            if not b.startswith('10'):
+                return False
     return True
