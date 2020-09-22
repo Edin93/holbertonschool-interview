8e769be1d626b1916bb2b61a6a7c4bca3c30b701
edin93 <il_matador@hotmail.fr>
2020-09-22 19:09:21 +0000

    update utf8 algo

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,7 @@ Contains a function that validate a utf-8 data.
 
 def intToBin(n):
     """Converts an integer to binary."""
-    b = bin(n)[2:]
-    return ('0' * (8 - len(b))) +  b
+    return '{:08b}'.format(n)
 
 
 def isValidByte(data):
@@ -28,31 +27,24 @@ def validUTF8(data):
         if bytes[i].startswith('0'):
             i += 1
         elif bytes[i][:3].startswith('110'):
-            if not (i + 2 <= limit):
-                return False
             tmp = bytes[i+1:i+2]
-            if not (tmp[0].startswith('10')):
+            if not (len(tmp) == 1):
+                return False
+            if not isValidByte(tmp):
                 return False
             i += 2
         elif bytes[i][:4].startswith('1110'):
-            if not (i + 3 <= limit):
-                return False
             tmp = bytes[i+1:i+3]
-            if not (
-                    tmp[0].startswith('10') and
-                    tmp[1].startswith('10')
-            ):
+            if not (len(tmp) == 2):
+                return False
+            if not isValidByte(tmp):
                 return False
             i += 3
         elif bytes[i][:5].startswith('11110'):
-            if not (i + 4 <= limit):
-                return False
             tmp = bytes[i+1:i+4]
-            if not (
-                    tmp[0].startswith('10') and
-                    tmp[1].startswith('10') and
-                    tmp[2].startswith('10')
-            ):
+            if not (len(tmp) == 3):
+                return False
+            if not isValidByte(tmp):
                 return False
             i += 4
         else:
