8e1deb2abb71404a3e31a9e8e364090e2db3ca3e
Edin93 <il_matador@hotmail.fr>
2020-11-18 04:19:40 +0100

    task star wars api.

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 3) {
+  const movieURL = 'https://swapi-api.hbtn.io/api/films';
+  const movieID = process.argv[2];
+  request(`${movieURL}/${movieID}`, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else if (res) {
+      const characters = JSON.parse(body).characters;
+      for (let i = 0; i < characters.length; i++) {
+        const personURL = characters[i];
+        request(personURL, function (perror, pres, pbody) {
+          if (perror) {
+            console.log(perror);
+          } else if (pres) {
+            console.log(JSON.parse(pbody).name);
+          }
+        });
+      }
+    }
+  });
+}
