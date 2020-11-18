0fd674c7436eed7f469eb63854b43027685bc80e
Edin93 <il_matador@hotmail.fr>
2020-11-18 13:16:49 +0100

    update function with async/await

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,25 +1,38 @@
 #!/usr/bin/node
 
 const request = require('request');
+let characters = [];
 
-if (process.argv.length === 3) {
-  const movieURL = 'https://swapi-api.hbtn.io/api/films';
-  const movieID = process.argv[2];
-  request(`${movieURL}/${movieID}`, function (err, res, body) {
-    if (err) {
-      console.log(err);
-    } else if (res.statusCode === 200) {
-      const characters = JSON.parse(body).characters;
-      for (let i = 0; i < characters.length; i++) {
-        const personURL = characters[i];
-        request(personURL, function (perror, pres, pbody) {
-          if (perror) {
-            console.log(perror);
-          } else if (pres.statusCode === 200) {
-            console.log(JSON.parse(pbody).name);
-          }
-        });
+function displayNames () {
+  if (process.argv.length === 3) {
+    const movieURL = 'https://swapi-api.hbtn.io/api/films';
+    const movieID = process.argv[2];
+    request(`${movieURL}/${movieID}`, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else if (response.statusCode === 200) {
+        characters = JSON.parse(body).characters;
+        for (let i = 0; i < characters.length; i++) {
+          (async function (personURL = characters[i]) {
+            const syncResponse = await syncRequest(personURL);
+            console.log(syncResponse);
+          })();
+        }
       }
-    }
-  });
+    });
+  }
 }
+
+const syncRequest = function (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        return reject(error);
+      } else if (response) {
+        return resolve(JSON.parse(body).name);
+      }
+    });
+  });
+};
+
+displayNames();
