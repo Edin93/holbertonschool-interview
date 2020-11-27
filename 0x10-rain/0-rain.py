464ffc8c0b80917d428aac83d35406319fa50720
Edin93 <il_matador@hotmail.fr>
2020-11-27 18:53:39 +0100

    update trapping rain algo

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,32 +7,43 @@ Module for calculating water retention after it rains.
 def rain(walls: list) -> int:
     """
     Calculates how much water will be retained after it rains.
-
     Parameters:
         walls (list): list of non-negative integers.
-
     Returns:
         (int): the amount of rainwater retained, as an integer.
     """
-    i = 0
-    j = 0
+    start = 0
+    end = 0
+    current_max = 0
     rain = 0
-    diff = 0
-    end = len(walls)
+    edges = []
+    n = len(walls)
 
-    i = j + 1
-    separation = 0
+    end = start + 1
+    current_max = end
+    while end < n:
+        while walls[start] > walls[end]:
+            end += 1
+            if walls[current_max] <= walls[end]:
+                current_max = end
+        # print('START = ', start)
+        # print('END = ', end)
+        # print('CURRENT MAX = ', current_max)
+        # print('---------------------------')
+        end = current_max
+        if end > start + 1:
+            edges.append((start, end))
+        start = end
+        end = start + 1
+        current_max = end
 
-    while i < end:
-        if walls[j] > walls[i]:
-            diff += walls[i]
-            separation += 1
-            i += 1
-        elif walls[j] <= walls[i] or i == end - 1:
-            rain += min(walls[i], walls[j]) * separation - diff
-            separation = 0
-            diff = 0
-            j = i
-            i += 1
+    # print('EDGES ==> ', edges)
+
+    for t in edges:
+        t0 = t[0]
+        t1 = t[1]
+        holding_wall = min(walls[t0], walls[t1])
+        for i in walls[t0 + 1: t1]:
+            rain += holding_wall - i
 
     return rain
