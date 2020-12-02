329204678bc8eaa82101abe908895074a57cd23b
Edin93 <il_matador@hotmail.fr>
2020-12-02 22:14:08 +0100

    add files

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * heap_sort - Sorts an array in the sift-down Heap sort algorithm.
+ *
+ * @array: the array to be sorted.
+ * @size: the length of the array.
+*/
+void heap_sort(int *array, size_t size)
+{
+}
