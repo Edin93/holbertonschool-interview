c6b371557e1b036b9efe9e5b7c238ff73146cfd0
Edin93 <il_matador@hotmail.fr>
2020-12-23 23:16:39 +0100

    heap extract init.

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -8,10 +8,20 @@ count as javascript, but java should not).
 import requests
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, after=None):
 	"""
 		Queries the Reddit API, parses the title of all hot articles, and
 		prints a sorted count of given keywords (case-insensitive, delimited
 		by spaces. Javascript should count as javascript, but java should not).
 	"""
-	
+	sub = subreddit.lower()
+	words = [w.lower() for w in word_list]
+	print(words)
+	params = {'limit': 100}
+	url = 'https://www.reddit.com/r/{}/hot'.format(sub)
+
+	r = requests.get(url, params=params, allow_redirects=False)
+	data = r.json()
+	print(data)
+	# data = r.json().get('data').get('children')
+	# print(data)
---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * heap_extract - Extracts the root node of a Max Binary Heap.
+ *
+ * @root: A double pointer to the root node of the heap.
+ *
+ * Return: The value stored in the root node otherwise return 0.
+*/
+int heap_extract(heap_t **root)
+{
+}
