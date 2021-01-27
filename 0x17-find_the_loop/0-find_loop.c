4f060b8239382199c330280fcb76c5ca7d58236d
Edin93 <il_matador@hotmail.fr>
2021-01-27 22:39:35 +0100

    task find loop done

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -13,4 +13,31 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *slow, *fast;
+
+	if (!head || !(head->next))
+		return (NULL);
+
+	slow = head->next;
+	fast = head->next->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+			break;
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (slow != fast)
+		return (NULL);
+
+	slow = head;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	return (slow);
 }
