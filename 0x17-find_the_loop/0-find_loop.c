2a54bafdede6c37d4eb85de7ac47431009ac71e5
Edin93 <il_matador@hotmail.fr>
2021-01-27 21:34:01 +0100

    find the loop init

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * find_listint_loop - Find the loop in a linked list.
+  *
+  * @head: Thea head of the linked list to search.
+  *
+  * Return: the address of the node where the loop starts, or NULL if there
+  * is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+}
